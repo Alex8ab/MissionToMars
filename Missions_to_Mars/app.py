e844b939d0bf44fcf5944e37ac84e0ee9812c2ea
Alejandro Ochoa <70457342+Alex8ab@users.noreply.github.com>
2021-03-26 14:32:51 -0600

    app completed

--- Missions_to_Mars/app.py
+++ Missions_to_Mars/app.py
@@ -1,1 +1,39 @@
+# Import Dependencies 
+from flask import Flask, render_template, redirect
+from flask_pymongo import PyMongo
+import scrape_mars
+import os
+
+# Instance Flask app
+app = Flask(__name__)
+
+# Mongo connection
+mongo = PyMongo(app, uri="mongodb://localhost:27017/scrape_mars_app")
+
+# Route render to index.html
+@app.route("/")
+def index(): 
+
+    # All the info stores
+    mars_info = mongo.db.mars_info.find_one()
+
+    # Return index and info
+    return render_template("index.html", mars_info = mars_info)
+
+# Route scrape Mars data 
+@app.route("/scrape")
+def scrape(): 
+
+    # Mars Data & Info
+    mars_info = mongo.db.mars_info
+    mars_data = scrape_mars.scrape_mars_news()
+    mars_data = scrape_mars.scrape_mars_image()
+    mars_data = scrape_mars.scrape_mars_facts()
+    mars_data = scrape_mars.scrape_mars_hemispheres()
+    mars_info.update({}, mars_data, upsert=True)
+
+    return redirect("/")
+
+if __name__ == "__main__": 
+    app.run(debug= True)
 
